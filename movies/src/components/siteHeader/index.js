1d3ea65b3ab7c6fcf99be74d8172a1913c2c7e38
Graham heeney <20102466@mail.wit.ie>
2024-11-21 13:54:36 +0000

    Removed header drop down and added a signin and signup option on the header.

--- movies/src/components/siteHeader/index.js
+++ movies/src/components/siteHeader/index.js
@@ -2,57 +2,30 @@ import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
-import MenuIcon from "@mui/icons-material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
 import { useNavigate } from "react-router-dom";
-import { styled } from '@mui/material/styles';
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { auth } from "../../firebase"; 
-import { signOut, onAuthStateChanged } from "firebase/auth"; 
-const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+import { styled } from "@mui/material/styles";
+import { auth } from "../../firebase";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 const SiteHeader = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [user, setUser] = useState(null); 
-  const open = Boolean(anchorEl);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-   
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user); 
+      setUser(user);
     });
-    
-    
     return () => unsubscribe();
   }, []);
 
-  const menuOptions = [
-    { label: "Home", path: "/" },
-    { label: "Favorites", path: "/movies/favorites" },
-    { label: "Top Rated Movies", path: "/movies/top-rated" },
-  ];
-
-  const handleMenuSelect = (pageURL) => {
-    navigate(pageURL, { replace: true });
-    setAnchorEl(null); 
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
   const handleSignOut = () => {
-    signOut(auth) // Sign out the user
+    signOut(auth)
       .then(() => {
         console.log("Signed out successfully");
-        navigate("/"); 
+        navigate("/");
       })
       .catch((error) => {
         console.log(error.message);
@@ -64,49 +37,34 @@ const SiteHeader = () => {
       <AppBar position="fixed" color="secondary">
         <Toolbar>
           <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
+            Graham's TMDB Client
           </Typography>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ marginRight: "20px" }}>
             All you ever wanted to know about Movies!
           </Typography>
-          <IconButton
-            aria-label="menu"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-            color="inherit"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={open}
-            onClose={() => setAnchorEl(null)}
-          >
-            {menuOptions.map((opt) => (
-              <MenuItem key={opt.label} onClick={() => handleMenuSelect(opt.path)}>
-                {opt.label}
-              </MenuItem>
-            ))}
-            {user ? (
-              <MenuItem onClick={handleSignOut}>Sign Out</MenuItem> 
-            ) : (
-              <>
-                <MenuItem onClick={() => navigate("/signup")}>Sign Up</MenuItem>
-                <MenuItem onClick={() => navigate("/login")}>Sign In</MenuItem> 
-              </>
-            )}
-          </Menu>
+          <Button color="inherit" onClick={() => navigate("/")}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/movies/favorites")}>
+            Favorites
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/movies/top-rated")}>
+            Top Rated Movies
+          </Button>
+          {user ? (
+            <Button color="inherit" onClick={handleSignOut}>
+              Sign Out
+            </Button>
+          ) : (
+            <>
+              <Button color="inherit" onClick={() => navigate("/signup")}>
+                Sign Up
+              </Button>
+              <Button color="inherit" onClick={() => navigate("/login")}>
+                Sign In
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
       <Offset />
